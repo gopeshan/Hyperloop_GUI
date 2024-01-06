7aa9b5a88a2220032c7152ca602ade16f7db0ef2
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-06 00:06:47 -0500

    Update script.js
    Added pod systems page button, shortcut button for pod systems, and alerts page main button. Added variables accordingly.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //function will run upon the completion of the websites loading, allowing all elements to be fully initialized before code acts on them 
 window.onload = function() {
 
+  //variables declared
+    let podStatus = "off"; //represents the stat of the pod 
   
 //start of page functionality, by hiding elements the single html file can appear to represent multiple pages 
     const homeButton = document.getElementById("homeLogo"); //home logo (acts as a button to return to the home page)
@@ -43,3 +45,28 @@ window.onload = function() {
       document.getElementById("dashboardMiddle").style.display = "flex"
     });
   
+  const alerts = document.getElementById("podSystem"); //pod systems page button
+    alerts.addEventListener('click', function() {
+      document.getElementById("menuSidebar").style.display = "none"
+      document.getElementById("menu-middle").style.display = "none"
+      document.getElementById("podsystemMiddle").style.display = "flex"
+      document.getElementById("podsystemSidebar").style.display = "flex"
+    });
+
+    const podStats = document.getElementById("podStatus"); //pod status shortcut button
+    podStats.addEventListener('click', function() {
+      document.getElementById("alertsMiddle").style.display = "none"
+      document.getElementById("inputsMiddle").style.display = "none"
+      document.getElementById("podsystemMiddle").style.display = "flex"
+      document.getElementById("podsystemSidebar").style.display = "flex"
+      document.getElementById("menuSidebar").style.display = "none"
+      document.getElementById("menu-middle").style.display = "none"
+      document.getElementById("dashboardMiddle").style.display = "none"
+    });
+
+    const inputs = document.getElementById("alerts"); //alerts page main button 
+    inputs.addEventListener('click', function() {
+      document.getElementById("menuSidebar").style.display = "none"
+      document.getElementById("menu-middle").style.display = "none"
+      document.getElementById("alertsMiddle").style.display = "flex"
+    });
