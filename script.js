b4b0e8f30fc5e4ea1c44681e210afb06804961c9
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-06 00:03:02 -0500

    Update script.js
    Added function to run upon website loading. Added a page functionality that hides elements. Added trip progress shortcut and dashboard main page button.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+//function will run upon the completion of the websites loading, allowing all elements to be fully initialized before code acts on them 
+window.onload = function() {
 
+  
+//start of page functionality, by hiding elements the single html file can appear to represent multiple pages 
+    const homeButton = document.getElementById("homeLogo"); //home logo (acts as a button to return to the home page)
+    homeButton.addEventListener('click', function() {
+      document.getElementById("alertsMiddle").style.display = "none"
+      document.getElementById("inputsMiddle").style.display = "none"
+      document.getElementById("podsystemMiddle").style.display = "none"
+      document.getElementById("podsystemSidebar").style.display = "none"
+      document.getElementById("menuSidebar").style.display = "flex"
+      document.getElementById("menu-middle").style.display = "flex"
+      document.getElementById("dashboardMiddle").style.display = "none"
+    });
+
+  const returnHome = document.getElementById("returnHome"); //return home shortcut
+    returnHome.addEventListener('click', function() {
+      document.getElementById("alertsMiddle").style.display = "none"
+      document.getElementById("inputsMiddle").style.display = "none"
+      document.getElementById("podsystemMiddle").style.display = "none"
+      document.getElementById("podsystemSidebar").style.display = "none"
+      document.getElementById("menuSidebar").style.display = "flex"
+      document.getElementById("menu-middle").style.display = "flex"
+      document.getElementById("dashboardMiddle").style.display = "none"
+    });
+
+    const tripProgress = document.getElementById("tripProgress"); //trip progress shortcut
+    tripProgress.addEventListener('click', function() {
+      document.getElementById("alertsMiddle").style.display = "none"
+      document.getElementById("inputsMiddle").style.display = "none"
+      document.getElementById("podsystemMiddle").style.display = "none"
+      document.getElementById("podsystemSidebar").style.display = "none"
+      document.getElementById("menuSidebar").style.display = "none"
+      document.getElementById("menu-middle").style.display = "none"
+      document.getElementById("dashboardMiddle").style.display = "flex"
+    });
+
+  const dashboard = document.getElementById("dashboard"); //dashboard main page button
+    dashboard.addEventListener('click', function() {
+      document.getElementById("menuSidebar").style.display = "none"
+      document.getElementById("menu-middle").style.display = "none"
+      document.getElementById("dashboardMiddle").style.display = "flex"
+    });
+  
